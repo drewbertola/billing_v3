904e4285ede33542f5f38dd4894c55472c163a0a
Drew Bertola <drewbertola@gmail.com>
2024-06-03 13:09:40 +0700

    fix migrations table names

--- database/migrations/2024_05_28_143255_create_line_items_table.php
+++ database/migrations/2024_05_28_143255_create_line_items_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('line_items', function (Blueprint $table) {
+        Schema::create('line_item', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('line_items');
+        Schema::dropIfExists('line_item');
     }
 };
